56fcc190f0c72d077b7316242231184fd307dc3d
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-20 04:04:13 +0900

    FIX :: getAtomDescription api 요청 함수 코드 수정

--- src/apis/getAtomDescription.api.ts
+++ src/apis/getAtomDescription.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import PreodicTableData from "../data/periodic-table.json";
 
 export async function getAtomDescription(id: number) {
-  if (id < 1 || id > 118) return "";
+  if (id < 1 || id > 119) return "";
   const name = PreodicTableData.elements[id - 1].name;
 
   const { data } = await axios.get("http://127.0.0.1:5000?name=" + name);
