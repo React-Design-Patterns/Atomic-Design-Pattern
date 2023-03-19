c8b7e24052d086928cb4a8afee4aaa160128b551
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-20 03:55:27 +0900

    DESIGN :: Header organism 높이 CSS 속성 수정

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -5,7 +5,7 @@ import Logo from "../../atoms/Logo";
 const Wrap = styled.div`
   width: 100%;
   min-width: 1440px;
-  height: 60px;
+  height: 52px;
   display: flex;
   align-items: center;
   justify-content: center;
