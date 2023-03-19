a78e37c1e50e125397f49b663d5a6eadcffe0b44
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-03-19 22:51:32 +0900

    FIX :: index.tsx 파일 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/globalStyle";
-import theme from "./styles/theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <>
     <GlobalStyle />
